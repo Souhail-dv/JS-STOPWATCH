86df3289449faa7c51552570400289d5cda55952
souhailad55 <souhailaaddidev@gmail.com>
2025-07-31 00:28:51 +0100

    Add start function to begin stopwatch timing

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ let timer = null;
 let startTime = 0;
 let elapsedTime = 0;
 let isRunning = false;
+
+
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
