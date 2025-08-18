eb3cae7d5daf83a888b6ef7d7494e00a09479ca2
souhailad55 <souhailaaddidev@gmail.com>
2025-08-18 13:53:41 +0100

    Pad seconds to two digits

--- main.js
+++ main.js
@@ -39,3 +39,4 @@ function update(){
     hours = String(hours).padStart(2, "0");
     minutes = String(minutes).padStart(2, "0");
     seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
